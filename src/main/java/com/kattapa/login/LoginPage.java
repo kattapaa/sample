6d2fb080d44b0b9a8d6691b135aa11c6b74f03d1
manee2k6 <vasanthbalaji@hotmail.com>
2018-02-28 06:46:57 +0530

    added project files

---
+++ src/main/java/com/kattapa/login/LoginPage.java
@@ -0,0 +1,11 @@
+package com.kattapa.login;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Login Page is executed......");
+
+	}
+
+}
---
+++ src/main/java/com/kattapa/register/RegisterPage.java
@@ -0,0 +1,12 @@
+package com.kattapa.register;
+
+public class RegisterPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Registration Page.....");
+
+	}
+
+}
